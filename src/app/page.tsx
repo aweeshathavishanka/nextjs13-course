ce2513fd5f9daf23e5e41162623968ed081d9981
aweeshathavishanka <aweesha.info@gmail.com>
2024-04-21 10:54:24 +0530

    Components with NextJS 14

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,23 @@
 "use client";
+import Header from "@/components/Header";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
   const router = useRouter();
   return (
-    <main className=" items-center justify-center text-center py-10">
-      <h1 className=" text-3xl">Main Page</h1>
-      <div className=" py-10">
-        <button
-          onClick={() => router.push("/profile")}
-          className=" text-white bg-blue-500 rounded-sm py-2 px-4">
-          Profile
-        </button>
+    <main>
+      <div className=" text-center">
+        <Header />
+      </div>
+      <div className=" items-center justify-center text-center py-10">
+        <h1 className=" text-3xl">Main Page</h1>
+        <div className=" py-10">
+          <button
+            onClick={() => router.push("/profile")}
+            className=" text-white bg-blue-500 rounded-sm py-2 px-4">
+            Profile
+          </button>
+        </div>
       </div>
     </main>
   );
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const Header = () => {
+  return (
+    <div className=" bg-blue-500 py-4  mb-4">
+      <h1 className=" text-3xl">This is my Header</h1>
+    </div>
+  );
+};
+
+export default Header;
