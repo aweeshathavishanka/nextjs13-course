7734b0571c79e2fea7670978b14b7220952fe7a3
aweeshathavishanka <aweesha.info@gmail.com>
2024-04-21 09:49:19 +0530

    Router with NextJS 14

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,10 +1,21 @@
 "use client";
 
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 
 export default function Profile() {
   const params = useParams();
-  console.log(params);
+  const router = useRouter();
 
-  return <div>My id profile Page</div>;
+  return (
+    <div className=" items-center text-center py-10">
+      <h1>Profile Page id: {params.id}</h1>
+      <div className=" py-4">
+        <button
+          onClick={() => router.push("/profile")}
+          className=" py-2 px-4 bg-blue-500">
+          Back Profile
+        </button>
+      </div>
+    </div>
+  );
 }
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+"use client";
+
+import { useRouter } from "next/navigation";
 
 const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  const router = useRouter();
+
+  console.log(router);
+  return (
+    <div>
+      <h1>my list of profiles</h1>
+      <div>
+        <ul>
+          <li
+            onClick={() => router.push("profile/1")}
+            className=" cursor-pointer">
+            Profile 01
+          </li>
+          <li
+            onClick={() => router.push("profile/2")}
+            className=" cursor-pointer">
+            Profile 02
+          </li>
+          <li
+            onClick={() => router.push("profile/3")}
+            className=" cursor-pointer">
+            Profile 03
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default ProfilePage;
