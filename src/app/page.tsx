61debeff18adb237a41f685e8c217bdc26200953
aweeshathavishanka <aweesha.info@gmail.com>
2024-04-21 09:54:45 +0530

    Customize Home Page / Add Button Naviagtion in Profile Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
-import Image from "next/image";
+"use client";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  return <main>Main Page of my application</main>;
+  const router = useRouter();
+  return (
+    <main className=" items-center justify-center text-center py-10">
+      <h1 className=" text-3xl">Main Page</h1>
+      <div className=" py-10">
+        <button
+          onClick={() => router.push("/profile")}
+          className=" text-white bg-blue-500 rounded-sm py-2 px-4">
+          Profile
+        </button>
+      </div>
+    </main>
+  );
 }
